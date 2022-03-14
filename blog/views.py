d0d31f2df14774c059605f7998c8d5dc1ab55204
yawtalkstechs <yawtalkstechs@gmail.com>
2022-03-14 15:44:25 +0000

    Updates

--- blog/views.py
+++ blog/views.py
@@ -8,23 +8,23 @@ from .models import Post
 # Create your views here.
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blog/home.html'
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 class BlogCreateView(CreateView):
     model = Post
-    template_name = 'post_new.html'
+    template_name = 'blog/post_new.html'
     fields = ['author', 'title', 'body']
 
 class BlogUpdateView(UpdateView):
     model = Post
-    template_name = 'post_edit.html'
+    template_name = 'blog/post_edit.html'
     fields = ['title', 'body']
 
 class BlogDeleteView(DeleteView):
     model = Post
-    template_name = 'post_delete.html'
+    template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('home')
