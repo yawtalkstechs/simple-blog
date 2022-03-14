7d8a3fb697b8d492b73605910df899aff117a525
yawtalkstechs <yawtalkstechs@gmail.com>
2022-03-14 20:44:31 +0000

    User accounts added

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+LOGIN_REDIRECT_URL ='home'
+LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
